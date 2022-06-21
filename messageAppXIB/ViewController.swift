17518a656799670ac7d37e4e00f6e254c1f00c8b
Yuchen <smirkiiiv@gmail.com>
2022-06-21 09:22:47 +0800

    Initial Commit

---
+++ messageAppXIB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  messageAppXIB
+//
+//  Created by 林祐辰 on 2022/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
